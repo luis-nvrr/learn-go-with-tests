c6c7a06a87412d08cf047e58015b0e112959f780
Luis Navarro <luisfnvrr@gmail.com>
2022-03-01 22:11:50 -0300

    feat: add templates

---
+++ 18.templating/blogrenderer/blogrenderer.go
@@ -0,0 +1,45 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+)
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type Post struct {
+	Title       string
+	Body        string
+	Description string
+	Tags        []string
+}
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
---
+++ 18.templating/blogrenderer/go.mod
@@ -0,0 +1,5 @@
+module github.com/luis-nvrr/blogrenderer
+
+go 1.17
+
+require github.com/approvals/go-approval-tests v0.0.0-20220217205408-02c48c44c897
